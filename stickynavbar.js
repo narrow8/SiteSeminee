341f602030ed4b66aea84aa19f33eb8544b5e6b2
Gogete Robert <reclaimer19mc@gmail.com>
2018-08-21 17:28:10 +0300

    Add files via upload

--- stickynavbar.js
+++ stickynavbar.js
@@ -4,7 +4,7 @@ var navbar = document.getElementById("meniu");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset > sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
