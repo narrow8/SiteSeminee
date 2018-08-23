e6ba1b1864876cd3836db15899ed4fc80f269a41
Gogete Robert <reclaimer19mc@gmail.com>
2018-08-23 20:13:19 +0300

    Add files via upload

--- cos.js
+++ cos.js
@@ -4,9 +4,15 @@ var k = 0;
 
 function add(date_semineu)
 {
+    /*
+    var seminee = JSON.parse(sessionStorage.getItem('seminee'));
+    var prices = JSON.parse(sessionStorage.getItem('prices'));
+    var k = JSON.parse(sessionStorage.getItem('k'));
+    */
     prices[k]=date_semineu.split('$')[1].split("Adauga in cos")[0];
     seminee[k]=date_semineu.split('$')[0];
     k++;
+
     sessionStorage.setItem('seminee', JSON.stringify(seminee));
     sessionStorage.setItem('prices', JSON.stringify(prices));
     sessionStorage.setItem('k', JSON.stringify(k));
@@ -35,17 +41,56 @@ function populate()
         semineu.classList.add("top-padding");
         row.appendChild(semineu);
         var pret = document.createElement("div");
-        var node = document.createTextNode(prices[i]);
+        var node = document.createTextNode('$' + prices[i]);
         pret.appendChild(node);
         pret.classList.add("col-sm-6");
         pret.classList.add("text-center");
         pret.classList.add("top-padding");
         row.appendChild(pret);
-        
+
         var delete_button = document.createElement("button");
         var node = document.createTextNode("Sterge");
         delete_button.appendChild(node);
-        delete_button.addEventListener("click", delete_row());
+        delete_button.addEventListener("click", function(){
+            document.getElementById(delete_button.parentElement.parentElement.id).remove();
+            var sem;
+            var pric;
+
+            var j = 0;
+
+            sem = delete_button.parentElement.parentElement.firstChild.textContent;
+            pric = delete_button.parentElement.parentElement.lastChild.textContent.split("$")[1].split("Sterge")[0];
+            var semin = new Array();
+            var pric2 = new Array();
+
+            var seminee2 = JSON.parse(sessionStorage.getItem('seminee'));
+            var prices2 = JSON.parse(sessionStorage.getItem('prices'));
+
+            var not_deleted = 1;
+
+            seminee2.forEach(semi => {
+                if (not_deleted == 0)
+                {
+                    semin[j] = semi;
+                    pric2[j] = pric;
+                    j++;
+                }
+                else
+                {
+                    if (semi.textContent == sem.textContent){
+                        semin[j] = semi;
+                        pric2[j] = pric;
+                        j++;
+                   }
+                   else
+                       not_deleted = 0;
+                }
+            });
+
+            sessionStorage.setItem('seminee', JSON.stringify(semin));
+            sessionStorage.setItem('prices', JSON.stringify(pric2));
+            sessionStorage.setItem('k', JSON.stringify(j));
+        });
         delete_button.classList.add("sterge");
         pret.appendChild(delete_button);
         total += parseInt(prices[i]);
@@ -63,7 +108,7 @@ function populate()
     semineu.classList.add("top-padding");
     row.appendChild(semineu);
     var pret = document.createElement("div");
-    var node = document.createTextNode(total);
+    var node = document.createTextNode('$' + total);
     pret.appendChild(node);
     pret.classList.add("col-sm-6");
     pret.classList.add("text-center");
@@ -73,9 +118,4 @@ function populate()
 
 document.body.onload = function(){
     populate();
-}
-
-function delete_row()
-{
-
 }
