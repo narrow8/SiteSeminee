57f2d182018bd9234c23bfd37fc4a1850fc0dcab
Gogete Robert <reclaimer19mc@gmail.com>
2018-08-26 13:57:07 +0300

    Add files via upload

--- stickynavbar.js
+++ stickynavbar.js
@@ -2,6 +2,7 @@ window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("meniu");
 var sticky = navbar.offsetTop;
+var contact = false;
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
@@ -10,3 +11,24 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+function showContact(){
+  if (location.pathname.includes("main.html"))
+    document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+  else
+  {
+    sessionStorage.setItem('contact', 1);
+    if (window.location.pathname.includes("focare") || window.location.pathname.includes("grile") || window.location.pathname.includes("cosuri") || window.location.pathname.includes("izolatii") || window.location.pathname.includes("accesorii"))
+      window.location = "../../main.html";
+    else
+      window.location = "main.html";
+  }
+}
+
+document.body.onload = function() {
+  if (sessionStorage.getItem('contact') == 1)
+  {
+    document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+    sessionStorage.setItem('contact', 0);
+  }
+};
